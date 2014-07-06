8432e1a81875779bc945ed3f85a94aee1efc23b2
Michael Bernstein <michael@fandomhome.com>
2014-07-06 15:46:27 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+"""`main` is the top level module for your Morepath application."""
+
+# Import the Morepath Framework
+import morepath
+
+app = morepath.App(name='Hello')
+# Note: We don't need to call run() since our application is embedded within
+# the App Engine WSGI application server.
+
+
+@app.path('')
+class Root(object):
+    pass
+
+
+@app.view(model=Root)
+def hello_world(self, request):
+    """Return a friendly HTTP greeting."""
+    return 'Hello World!'
+
+
+#@app.errorhandler(404)
+#def page_not_found(e):
+    #"""Return a custom 404 error."""
+    #return 'Sorry, Nothing at this URL.', 404
+
+
+#@app.errorhandler(500)
+#def page_not_found(e):
+    #"""Return a custom 500 error."""
+    #return 'Sorry, unexpected error: {}'.format(e), 500
