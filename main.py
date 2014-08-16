c88af4967e4ad99c137dda16d84efc5b871fe74a
Michael R. Bernstein <michael@fandomhome.com>
2014-08-16 07:37:38 -0500

    Better error handling.

--- main.py
+++ main.py
@@ -2,33 +2,36 @@
 
 # Import the Morepath Framework
 import morepath
+from webob.exc import HTTPNotFound, HTTPInternalServerError
 
 app = morepath.App(name='Hello')
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
 
-
 @app.path('')
 class Root(object):
     pass
-
 
 @app.view(model=Root)
 def hello_world(self, request):
     """Return a friendly HTTP greeting."""
     return 'Hello World!'
 
+@app.view(model=HTTPNotFound)
+def notfound_custom(self, request):
+    """REturn a custom 404 error"""
+    def set_status_code(response):
+        response.status_code = self.code # pass along 404
+    request.after(set_status_code)
+    return "Sorry, Nothing at this URL."
+    
+@app.view(model=HTTPInternalServerError)
+def servererror_custom(self, request):
+    def set_status_code(response):
+        response.status_code = self.code # pass along 500
+    request.after(set_status_code)
+    return "Sorry, unexpected error: {}".format(self.detail)
+
 config = morepath.setup()
 config.scan()
 config.commit()
-
-#@app.errorhandler(404)
-#def page_not_found(e):
-    #"""Return a custom 404 error."""
-    #return 'Sorry, Nothing at this URL.', 404
-
-
-#@app.errorhandler(500)
-#def page_not_found(e):
-    #"""Return a custom 500 error."""
-    #return 'Sorry, unexpected error: {}'.format(e), 500
